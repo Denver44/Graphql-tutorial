5143a2de033cba412337202b5d1950aff45518e7
Durgesh <durgeshrai2012@gmail.com>
2021-01-25 01:07:04 +0530

    delte functionality added

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ const Student = require("./models/students");
 // MIDDLEWARE
 app.use(express.json());
 
-// API ROUTE
+// API ROUTES
 
 // As we have to create Student Data so we will use POST.
 
+// Create Route
 app.post("/students", async (req, res) => {
   try {
     const user = new Student(req.body);
@@ -22,6 +23,7 @@ app.post("/students", async (req, res) => {
   }
 });
 
+// Get Route
 app.get("/students", async (req, res) => {
   try {
     const StudentData = await Student.find();
@@ -31,8 +33,22 @@ app.get("/students", async (req, res) => {
   }
 });
 
-// get an indvidula student data
+// get an Individual student data
+app.get("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id; // through this we get the id from the url
+    const studentData = await Student.findById(_id); // we get the data by findById method.
+    if (!studentData) {
+      return res.status(404).send();
+    } else {
+      res.status(200).send(studentData);
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
+// Update Route
 app.patch("/students/:id", async (req, res) => {
   try {
     const _id = req.params.id; // through this we get the id from the url
@@ -44,15 +60,16 @@ app.patch("/students/:id", async (req, res) => {
     res.status(404).send(e);
   }
 });
-app.get("/students/:id", async (req, res) => {
+
+// Delete Route
+app.delete("/students/:id", async (req, res) => {
   try {
-    const _id = req.params.id; // through this we get the id from the url
-    const studentData = await Student.findById(_id); // we get the data by findById method.
-    if (!studentData) {
+    const deleteStudentData = await Student.findByIdAndDelete(req.params.id); // we get the data by findById method.
+    console.log(deleteStudentData);
+    if (!deleteStudentData) {
       return res.status(404).send();
-    } else {
-      res.status(200).send(studentData);
     }
+    res.status(200).send(deleteStudentData);
   } catch (e) {
     res.status(500).send(e);
   }
