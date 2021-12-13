4cf6aeb0560732594634475228c9e04c4f9e99a7
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-13 10:24:29 +0530

    companies Queries Completed & bidirectional also

--- schema/schema.js
+++ schema/schema.js
@@ -1,15 +1,30 @@
 import graphql from "graphql";
 import _ from "lodash";
 import axios from "axios";
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+} = graphql;
 
+// Here put the values as an arrow function it will solve our js problem that object not defined otherwise it will throw an error of UserType not defined.
 const CompanyType = new GraphQLObjectType({
   name: "Company",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     desc: { type: GraphQLString },
-  },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/user`)
+          .then((res) => res.data);
+      },
+    },
+  }),
 });
 
 const UserType = new GraphQLObjectType({
@@ -20,6 +35,11 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt },
     company: {
       type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((res) => res.data);
+      },
     },
   },
 });
@@ -35,6 +55,17 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         return axios
           .get(`http://localhost:3000/user/${args.id}`)
+          .then((res) => res.data);
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
           .then((res) => res.data);
       },
     },
